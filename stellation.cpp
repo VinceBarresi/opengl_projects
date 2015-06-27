f5a0bd5ff5fdbcce7e127a227d5d9b2fa7cf4ec5
Vincent Barresi <vbarresi@amplify.com>
2015-06-27 12:22:20 -0400

    adjust formatting

--- stellation.cpp
+++ stellation.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <glut.h>
 
+#define PI 3.14159265
 #define TWOPI 2*3.14159265
 
 Point2 CP1;
@@ -13,17 +14,17 @@ const int screenHeight = 480; // height of screen window in pixels
 void stellation(int n,float radius, int rotAngle)
 {
    if (n < 5) return; //bad number of sides
-    double angle = rotAngle*3.1415926/180.0;  //initial angle
-    float angleInc = 2*3.142/n; //angle increment
+    double angle = rotAngle * PI / 180.0;  //initial angle
+    float angleInc = TWOPI / n; //angle increment
     float cx, cy;
-    cx = radius*cos(angle);
-    cy = radius*sin(angle);
+    cx = radius * cos(angle);
+    cy = radius * sin(angle);
     cvs.moveRel(cx, cy);
-    for(int k=0;k<n;k++)
+    for(int k = 0; k < n; k++)
     {
-      angle+=2*angleInc;
-      cx = radius*cos(angle);
-      cy = radius*sin(angle);
+      angle += 2 * angleInc;
+      cx = radius * cos(angle);
+      cy = radius * sin(angle);
       cvs.lineRel(cx, cy);
     }
     cvs.moveRel(-cx, -cy);
@@ -31,17 +32,17 @@ void stellation(int n,float radius, int rotAngle)
 
 float fx(float t1)
 { 
-  return 0.3*t1*t1;
+  return 0.3 * t1 * t1;
 }
 
 float fy(float t2)
 {
-  return 2*0.3*t2;
+  return 2 * 0.3 * t2;
 }
-//<<<<<<<<<<<<<<<<<<<<<<< myInit >>>>>>>>>>>>>>>>>>>>
+
 void myInit(void)
 {
-  glClearColor(0.0,0.0,0.0,0.0); // background color is white
+  glClearColor(0.0, 0.0, 0.0, 0.0); // background color is white
   glColor3f(0.0f, 0.0f, 0.0f); // drawing color is black
   glPointSize(4.0); // a 'dot' is 2 by 2 pixels
   glMatrixMode(GL_PROJECTION); // set "camera shape"
@@ -50,25 +51,23 @@ void myInit(void)
   glLoadIdentity();
 }
 
-//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
 void myDisplay(void)
 { 
   glClear(GL_COLOR_BUFFER_BIT);
   cvs.setWindow(-30.0, 30.0, -30.0, 30.0); 
   cvs.setViewport(0, 640, 0, 480); 
-  GLdouble len=0.002;
+  GLdouble len = 0.002;
   
   cvs.setWindow(-10.0, 10.0, -10.0, 10.0); 
   cvs.setViewport(0, 540, 0, 480); 
     
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3f(1,0,0);
-  stellation(7,6,20);
+  glColor3f(1, 0, 0);
+  stellation(7, 6, 20);
 
 }
 
-//<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
 void main(int argc, char** argv)
 {
   glutCreateWindow("Stellation"); // open the screen window
