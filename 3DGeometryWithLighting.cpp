726a38b9c6573bfc2bea9a601405115974c98f6d
Vincent Barresi <vbarresi@amplify.com>
2015-07-19 17:05:07 -0400

    Add 3D Geometry with Lighting application

---
+++ 3DGeometryWithLighting.cpp
@@ -0,0 +1,130 @@
+#include <GL/glut.h>
+#include <cmath>
+
+GLfloat WHITE[] = {1, 1, 1};
+GLfloat BLACK[] = {0, 0, 0};
+
+class Camera {
+  double theta;      // determines the x and z positions
+  double y;          // the current y position
+  double dTheta;     // increment in theta for swinging the camera around
+  double dy;         // increment in y for moving the camera up/down
+public:
+  Camera(): theta(0), y(10), dTheta(0.04), dy(0.6) {} 
+  double getX() {return 10 * cos(theta);}
+  double getY() {return y;}
+  double getZ() {return 10 * sin(theta);}
+  void moveRight() {theta += dTheta;}
+  void moveLeft() {theta -= dTheta;}
+  void moveUp() {y += dy;}
+  void moveDown() {if (y > dy) y -= dy;}
+};
+
+class Checkerboard {
+  int displayListId;
+  int width;
+  int depth;
+public:
+  Checkerboard(int width, int depth): width(width), depth(depth) {}
+  double centerx() {return width / 2;}
+  double centerz() {return depth / 2;}
+  void create() {
+    displayListId = glGenLists(1);
+    glNewList(displayListId, GL_COMPILE);
+    GLfloat lightPosition[] = {4, 3, 7, 1};
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+    glBegin(GL_QUADS);
+    glNormal3d(0, 1, 0);
+    for (int x = 0; x < width - 1; x++) {
+      for (int z = 0; z < depth - 1; z++) {
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE,(x + z) % 2 == 0 ? BLACK : WHITE);
+        glVertex3d(x, 0, z);
+        glVertex3d(x+1, 0, z);
+        glVertex3d(x+1, 0, z+1);
+        glVertex3d(x, 0, z+1);
+      }
+    }
+    glEnd();
+    glEndList();
+  }
+  void draw() {
+    glCallList(displayListId);
+  }
+};
+
+Checkerboard checkerboard(8, 8);
+Camera camera;
+
+void init() {
+  glEnable(GL_DEPTH_TEST);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, WHITE);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, WHITE);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, WHITE);
+  glMaterialf(GL_FRONT, GL_SHININESS, 30);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  checkerboard.create();
+}
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+  gluLookAt(camera.getX(), camera.getY(), camera.getZ(),checkerboard.centerx(), 0.0, checkerboard.centerz(),0.0, 1.0, 0.0);
+  checkerboard.draw();
+
+  glPushMatrix();
+  glTranslatef(2.75, .5, 2.0);    
+  glRotatef(45.0, 1.0, 0.0, 0.0);
+  glutSolidTorus(0.275, 0.85, 16, 40);
+  glPopMatrix();
+
+  glPushMatrix();
+  glTranslatef(3.75, .5, 2.0);    
+  glRotatef(45.0, 1.0, 0.0, 0.0);
+  glutSolidTorus(0.275, 0.85, 16, 40);
+  glPopMatrix();
+
+  glPushMatrix();
+  glTranslatef(5.75, 0.0, 3.0);               glRotatef(270.0, 1.0, 0.0, 0.0);
+  glutSolidCone(1.0, 2.0, 70, 12);
+  glPopMatrix();
+
+  glPushMatrix();
+  glTranslatef(5.75, .5, 5.0);        
+  glutSolidSphere(1.0, 30, 30);
+  glPopMatrix();
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+void reshape(GLint w, GLint h) {
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(40.0, GLfloat(w) / GLfloat(h), 1.0, 150.0);
+  glMatrixMode(GL_MODELVIEW);
+}
+
+void special(int key, int, int) {
+  switch (key) {
+    case GLUT_KEY_LEFT: camera.moveLeft(); break;
+    case GLUT_KEY_RIGHT: camera.moveRight(); break;
+    case GLUT_KEY_UP: camera.moveUp(); break;
+    case GLUT_KEY_DOWN: camera.moveDown(); break;
+  }
+  glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowPosition(80, 80);
+  glutInitWindowSize(800, 600);
+  glutCreateWindow("Barresi CSC706 Project 2");
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutSpecialFunc(special);
+  init();
+  glutMainLoop();
+}
